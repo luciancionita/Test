6d01b222120ef907d374f85e39cddc5cb682623f
luciancionita <lucian@scaleit.ro>
2015-07-13 13:13:58 +0300

    frm

--- VS test/TestGit/Form1.cs
+++ VS test/TestGit/Form1.cs
@@ -20,6 +20,10 @@ namespace TestGit
 
         private void Form1_Load(object sender, EventArgs e)
         {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
             MessageBox.Show("Test");
         }
     }
